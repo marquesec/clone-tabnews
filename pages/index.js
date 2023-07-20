8e1a2c0a54f3fd539be2fb0d415e1d48f1027f68
AndreMarques <57641185+marquesec@users.noreply.github.com>
2023-07-20 21:45:15 +0000

    adicionando o diretorio pages

---
+++ pages/index.js
@@ -0,0 +1,7 @@
+function Home() {
+    return <h1><i>Mensagem de boas vindas da aplicacao <b>:O</b></i></h1>
+}
+
+//exportar essa função Home para fora do arquivo dada para o next JS renderizar a página? export
+
+export default Home;
