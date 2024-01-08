3378af3d859487ba4c9d5f6ff199e9e75bf21741
AndreMarques <57641185+marquesec@users.noreply.github.com>
2024-01-08 23:39:42 +0000

    salvaldo problema pretty

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,11 +1,12 @@
-import database from "infra/database.js";
+import database from "infra/database";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1 as sum;");
-  console.log(result.rows);
-  response
-    .status(200)
-    .json({ chave: "alunos do curso.dev são pessoas acima da média" });
-}
+  const updatedAt = new Date().toISOString();
 
-export default status;
+  const databaseVersionResult = await database.query("SELECT version();");
+  console.log(databaseVersionResult.rows);
+
+  response.status(200).json({
+    updated_at: updatedAt,
+  });
+}
