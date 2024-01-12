a2bf70745b84c012749df4e4fb34ce5070bbcd61
AndreMarques <57641185+marquesec@users.noreply.github.com>
2024-01-12 00:56:44 +0000

    sql injection

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -5,10 +5,12 @@ async function status(request, response) {
 
   const databaseVersionResult = await database.query("SHOW server_version;");
   const databaseVersionValue = databaseVersionResult.rows[0].server_version;
+  console.log(databaseVersionResult);
 
   const databaseMaxConnectionsResult = await database.query(
     "SHOW max_connections;",
   );
+
   const databaseMaxConnectionsValue =
     databaseMaxConnectionsResult.rows[0].max_connections;
 
