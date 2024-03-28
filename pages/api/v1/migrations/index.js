85c022beb717ff95127f2d958736eabd426f22cc
AndreMarques <57641185+marquesec@users.noreply.github.com>
2024-03-28 22:01:17 +0000

    andre push backup 2 - 28/03

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -3,20 +3,28 @@ import { join } from "node:path";
 
 export default async function migrations(request, response) {
   if (request.method === "GET") {
-    console.log("Entrou no GET");
+    const migrations = await migrationRunner({
+      databaseUrl: process.env.DATABASE_URL,
+      dryRun: true,
+      dir: join("infra", "migrations"),
+      direction: "up",
+      verbose: true,
+      migrationsTable: "pgmigrations",
+    });
+    return response.status(200).json(migrations);
   }
 
   if (request.method === "POST") {
-    console.log("Entrou no POST");
+    const migrations = await migrationRunner({
+      databaseUrl: process.env.DATABASE_URL,
+      dryRun: false,
+      dir: join("infra", "migrations"),
+      direction: "up",
+      verbose: true,
+      migrationsTable: "pgmigrations",
+    });
+    return response.status(200).json(migrations);
   }
 
-  const migrations = await migrationRunner({
-    databaseUrl: process.env.DATABASE_URL,
-    dryRun: true,
-    dir: join("infra", "migrations"),
-    direction: "up",
-    verbose: true,
-    migrationsTable: "pgmigrations",
-  });
-  response.status(200).json(migrations);
+  return response.status(405).end();
 }
