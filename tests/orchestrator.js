aec89b19096f7b26252d689a96a2d005337d22ce
AndreMarques <57641185+marquesec@users.noreply.github.com>
2025-03-05 23:08:12 +0000

    Adicionando arquivos comentarios MAX + concurrently 8.2.2 - 05/03/2025

--- tests/orchestrator.js
+++ tests/orchestrator.js
@@ -1,19 +1,32 @@
 import retry from "async-retry";
+import database from "infra/database.js";
 
 async function waitForAllServices() {
   await waitForWebServer();
 
   async function waitForWebServer() {
-    return retry(fetchStatusPage);
+    return retry(fetchStatusPage, {
+      retries: 100,
+      maxTimeout: 1000,
+    });
 
-    async function fetchStatusPage(bail, tryNumber) {
-      console.log(tryNumber);
+    async function fetchStatusPage() {
       const response = await fetch("http://localhost:3000/api/v1/status");
-      const responseBody = await response.json();
+
+      if (response.status !== 200) {
+        throw Error();
+      }
     }
   }
 }
 
-export default {
+async function clearDatabase() {
+  await database.query("drop schema public cascade; create schema public;");
+}
+
+const orchestrator = {
   waitForAllServices,
+  clearDatabase,
 };
+
+export default orchestrator;
