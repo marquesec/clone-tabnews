64304e9c490770bdaa32059926fa8cf613b2fcc8
AndreMarques <57641185+marquesec@users.noreply.github.com>
2025-03-05 22:52:44 +0000

    Adicionando arquivos comentarios MAX + concurrently 8.2.2 - 05/03/2025

--- tests/orchestrator.js
+++ tests/orchestrator.js
@@ -6,7 +6,8 @@ async function waitForAllServices() {
   async function waitForWebServer() {
     return retry(fetchStatusPage);
 
-    async function fetchStatusPage() {
+    async function fetchStatusPage(bail, tryNumber) {
+      console.log(tryNumber);
       const response = await fetch("http://localhost:3000/api/v1/status");
       const responseBody = await response.json();
     }
