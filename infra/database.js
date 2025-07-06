49bcb81b391d1c7545280317036d1675364f1982
AndreMarques <57641185+marquesec@users.noreply.github.com>
2025-07-06 23:29:57 +0000

    fix: fix: insert proposital user error to test

--- infra/database.js
+++ infra/database.js
@@ -22,7 +22,7 @@ async function getNewClient() {
   const client = new Client({
     host: process.env.POSTGRES_HOST,
     port: process.env.POSTGRES_PORT,
-    user: process.env.POSTGRES_USER,
+    user: process.env.POSTGRES_USER2,
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
     ssl: getSSLValues(),
