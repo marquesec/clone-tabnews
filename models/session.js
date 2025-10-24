58e32a685a1b4a7e4042f9670503bad4ef2ff64c
AndreMarques <57641185+marquesec@users.noreply.github.com>
2025-10-24 15:12:27 +0000

    fix: fix

--- models/session.js
+++ models/session.js
@@ -1,66 +1,3 @@
-import crypto from "node:crypto";
-import database from "infra/database.js";
-import { UnauthorizedError } from "infra/errors";
-
-const EXPIRATION_IN_MILLISECONDS = 60 * 60 * 24 * 30 * 1000; // 30 Days
-
-async function findOneValidByToken(sessionToken) {
-  // CORREÇÃO APLICADA AQUI: O try/catch foi removido
-  const sessionFound = await runSelectQuery(sessionToken);
-
-  return sessionFound;
-
-  async function runSelectQuery(sessionToken) {
-    const results = await database.query({
-      text: `
-        SELECT
-          *
-        FROM
-          sessions
-        WHERE
-          token = $1
-          AND expires_at > NOW()
-        LIMIT
-          1
-      ;`,
-      values: [sessionToken],
-    });
-
-    if (results.rowCount === 0) {
-      throw new UnauthorizedError({
-        message: "Usuário não possui sessão ativa.",
-        action: "Verifique se este usuário está logado e tente novamente.",
-      });
-    }
-
-    return results.rows[0];
-  }
-}
-
-async function create(userId) {
-  const token = crypto.randomBytes(48).toString("hex");
-  const expiresAt = new Date(Date.now() + EXPIRATION_IN_MILLISECONDS);
-
-  const newSession = await runInsertQuery(token, userId, expiresAt);
-  return newSession;
-
-  async function runInsertQuery(token, userId, expiresAt) {
-    const results = await database.query({
-      text: `
-        INSERT INTO
-          sessions (token, user_id, expires_at)
-        VALUES
-          ($1, $2, $3)
-        RETURNING
-          *
-      ;`,
-      values: [token, userId, expiresAt],
-    });
-
-    return results.rows[0];
-  }
-}
-
 async function renew(sessionId) {
   const expiresAt = new Date(Date.now() + EXPIRATION_IN_MILLISECONDS);
 
@@ -79,5 +16,10 @@ async function renew(sessionId) {
           id = $1
         RETURNING
           *
-        ;`,
-      values: [sessionId, expiresAt],
+        ;`, // Linhas 71 a 82
+      values: [sessionId, expiresAt], // Linha 83 (a linha de erro)
+    }); // <--- PROVAVELMENTE FALTOU UM FECHAMENTO AQUI
+
+    return results.rows[0];
+  }
+}
