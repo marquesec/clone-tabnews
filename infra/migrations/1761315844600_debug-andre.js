8f57c6a2339a98af0f789e31889f5717e2249adf
AndreMarques <57641185+marquesec@users.noreply.github.com>
2025-10-24 15:36:41 +0000

    fix: fix

---
+++ infra/migrations/1761315844600_debug-andre.js
@@ -0,0 +1,18 @@
+/**
+ * @type {import('node-pg-migrate').ColumnDefinitions | undefined}
+ */
+exports.shorthands = undefined;
+
+/**
+ 
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+exports.up = (pgm) => {};
+
+/**
+
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+exports.down = (pgm) => {};
--- jest.config.js
+++ jest.config.js
@@ -1,17 +1,31 @@
 const dotenv = require("dotenv");
+
 dotenv.config({
+
   path: ".env.development",
+
 });
+
+
 
 const nextJest = require("next/jest");
 
+
+
 const createJestConfig = nextJest({
+
   dir: ".",
+
 });
+
 const jestConfig = createJestConfig({
+
   moduleDirectories: ["node_modules", "<rootDir>"],
-  // Aumentado de 18000 para 30000 milissegundos (30 segundos)
-  testTimeout: 30000, 
+
+  testTimeout: 30000,
+
 });
+
+
 
 module.exports = jestConfig;
